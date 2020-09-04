52ca10f939eb157413e607c1d67ddc3df99eca64
Serena Zhao <szhao5@ualberta.ca>
2020-09-04 10:05:00 -0600

    dickens corpus only

--- app/modes/default_mode.js
+++ app/modes/default_mode.js
@@ -1,23 +1,37 @@
 settings.corpora = {};
 settings.corporafolders = {};
 
-settings.corporafolders.sweac = {
-    title: "Akademiska texter",
-    contents: ["sweachum", "sweacsam"],
-    description: "A description"
-};
+// settings.corporafolders.sweac = {
+//     title: "Akademiska texter",
+//     contents: ["sweachum", "sweacsam"],
+//     description: "A description"
+// };
 
 /*
  * PRESELECTED CORPORA
  * Folders will be expanded to all corpora. Optionally prefix folders with __ , which will be ignored.
  */
-settings.preselectedCorpora = ["suc3"];
+settings.preselectedCorpora = ["dickens"];
 
 
-settings.corpora["magmakolumner"] = {
-    id: "magmakolumner",
-    title: "Magma kolumner 2009–2012",
-    description: "Material ur kolumner publicerade av <a target=\"_blank\" href=\"http://www.magma.fi\">Tankesmedjan Magma</a>",
+// settings.corpora["magmakolumner"] = {
+//     id: "magmakolumner",
+//     title: "Magma kolumner 2009–2012",
+//     description: "Material ur kolumner publicerade av <a target=\"_blank\" href=\"http://www.magma.fi\">Tankesmedjan Magma</a>",
+//     within: spWithin,
+//     context: spContext,
+//     attributes: modernAttrs,
+//     structAttributes: {
+//         text_author: {label: "author"},
+//         text_title: {label: "title"},
+//         text_date: {label: "date"}
+//     }
+// };
+
+settings.corpora["dickens"] = {
+    id: "dickens",
+    title: "Dickens Test Corpus",
+    description: "an English test corpus",
     within: spWithin,
     context: spContext,
     attributes: modernAttrs,
@@ -27,5 +41,8 @@ settings.corpora["magmakolumner"] = {
         text_date: {label: "date"}
     }
 };
+
+
+
 
 settings.corpusListing = new CorpusListing(settings.corpora);
