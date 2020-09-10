7c7253303edeea6f686a3dccd35861bac55df7ae
aarppe <antti.arppe@iki.fi>
2020-09-09 18:03:32 -0600

    Update default_mode.js
    Incorporated "old" config.js specifications for the Ahenakew-Wolfart texts.
    
    Left Dickens test corpus for the time being, for testing and validation purposes.

--- app/modes/default_mode.js
+++ app/modes/default_mode.js
@@ -7,11 +7,16 @@ settings.corporafolders = {};
 //     description: "A description"
 // };
 
+settings.corporafolders.crk = {
+    title : "nêhiyawêwin (Plains Cree) texts",
+    contents : ["wolfart_ahenakew"]
+};
+
 /*
  * PRESELECTED CORPORA
  * Folders will be expanded to all corpora. Optionally prefix folders with __ , which will be ignored.
  */
-settings.preselectedCorpora = ["dickens"];
+settings.preselectedCorpora = ["wolfart_ahenakew"];
 
 
 // settings.corpora["magmakolumner"] = {
@@ -27,6 +32,29 @@ settings.preselectedCorpora = ["dickens"];
 //         text_date: {label: "date"}
 //     }
 // };
+
+settings.corpora["wolfart_ahenakew"] = {
+        id : "wolfart_ahenakew",
+        title : "Ahenakew-Wolfart Texts",
+        description : "Plains Cree texts compiled and edited by H. C. Wolfart and Freda Ahenakew",
+        // limited_access : true, // This will be invoked once we get basic search functionality working
+        within : settings.defaultWithin, // According to Korp Frontend specs
+        context : settings.spContext, // According to Korp Frontend specs
+        attributes : {
+                msd : attrs.msd,
+                gloss : attrs.gloss, // This may need to be specified some/elsewhere, as it is ALTLab-specific
+                lemma : attrs.baseform,
+                dep : attrs.dep,
+        },
+        struct_attributes : {
+                text_author : {label : "author"},
+                text_title : {label : "title"},
+                text_title1 : {label : "text_title1"},
+                text_title2 : {label : "text_title2"},
+                text_lang : {label : "lang"},
+                /* text_url : {label : "url"}, */
+        }
+};
 
 settings.corpora["dickens"] = {
     id: "dickens",
